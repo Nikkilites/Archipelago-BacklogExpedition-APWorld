441dcaa06584f0a4dc84e248c198907ed518a097
Nikkilite <Niko6697n@gmail.com>
2025-10-26 11:08:07 +0100

    Added all mguffins and an extra filler item

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
 extra_regions = ["Desert", "Tropical", "Volcanic", "Polar", "Gemstone", "Mountainous", "Cherry Forest", "Swamp", "Jungle", "Pine Forest", "Orchard", "Plains", "Glacier Caves", "Tar Pits", "Candy", "Christmas"]
 monsters = ["Goblin","Troll","Slime"]
 events = ["Chest","Golden Chest"]
-fillers = ["Barrel of Mead", "Draugr's Bone", "Jotun's Toenail", "Nidhogg's Claw", "Shard of the Bifrost", "Jormungandr's Scale", "Fenrir's Fang", "Leaf of Yggdrasil", "Root of Yggdrasil", "Ratatoskr's Acorn", "Hugin's Feather", "Odin's Eyepatch", "Fafnir's Coin", "Baldur's Arrow", "Heimdall's Horn", "Lock of Freya's Hair", "Loki's Dagger", "Thor's Spare Hammer", "Sleipnir's Saddle"]
+fillers = ["Broken Rune", "Barrel of Mead", "Draugr's Bone", "Jotun's Toenail", "Nidhogg's Claw", "Shard of the Bifrost", "Jormungandr's Scale", "Fenrir's Fang", "Leaf of Yggdrasil", "Root of Yggdrasil", "Ratatoskr's Acorn", "Hugin's Feather", "Odin's Eyepatch", "Fafnir's Coin", "Baldur's Arrow", "Heimdall's Horn", "Lock of Freya's Hair", "Loki's Dagger", "Thor's Spare Hammer", "Sleipnir's Saddle"]
 
 # Has to be in the same order as the extra regions. The first one will be used for the starting island, so there needs to be +1
-mcguffins = ["Desert Bracelet", "Tropical Amulet", "Volcanic Jewel", "Polar Shard", "Gemstone Heirloom", "Mountainous Rock"]
+mcguffins = ["Desert Talisman", "Tropical Amulet", "Fire Ring", "Ice Shard", "Gemstone Crown", "Climbing Gloves", "Cherry Wand", "Swamp Waders", "Jungle Staff", "Pinecone Helmet", "Orchard Sickle", "Plains Greaves", "Glacier Diadem", "Tar Bracelet", "Candycane", "Christmas Present"]
