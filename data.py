9f1797b2fa2fd16f3a28919cb1af55b31fa9dfdb
Nikkilite <Niko6697n@gmail.com>
2025-10-26 02:12:46 +0200

    Add more data

--- data.py
+++ data.py
@@ -1,7 +1,7 @@
-extra_regions = ["Lava", "Ice", "Forest"]
+extra_regions = ["Desert", "Tropical", "Volcanic", "Polar", "Gemstone", "Mountainous", "Cherry Forest", "Swamp", "Jungle", "Pine Forest", "Orchard", "Plains", "Glacier Caves", "Tar Pits"]
 monsters = ["Goblin","Troll","Slime"]
 events = ["Chest","Golden Chest"]
 fillers = ["Barrel of Mead", "Draugr's Bone", "Jotun's Toenail", "Nidhogg's Claw", "Shard of the Bifrost", "Jormungandr's Scale", "Fenrir's Fang", "Leaf of Yggdrasil", "Root of Yggdrasil", "Ratatoskr's Acorn", "Hugin's Feather", "Odin's Eyepatch", "Fafnir's Coin", "Baldur's Arrow", "Heimdall's Horn", "Lock of Freya's Hair", "Loki's Dagger", "Thor's Spare Hammer", "Sleipnir's Saddle"]
 
 # Has to be in the same order as the extra regions. The first one will be used for the starting island, so there needs to be +1
-mcguffins = ["Standard Bracelet", "Lava Ring", "Ice Jewel", "Forest Amulet"]
+mcguffins = ["Desert Bracelet", "Tropical Amulet", "Volcanic Jewel", "Polar Shard", "Gemstone Heirloom", "Mountainous Rock"]
