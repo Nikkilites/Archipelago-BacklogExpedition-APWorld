efec036437c598f35b92bf5e3f5b4739fdee3fe6
Nikkilite <Niko6697n@gmail.com>
2025-10-26 10:56:58 +0100

    Added two extra island types

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-extra_regions = ["Desert", "Tropical", "Volcanic", "Polar", "Gemstone", "Mountainous", "Cherry Forest", "Swamp", "Jungle", "Pine Forest", "Orchard", "Plains", "Glacier Caves", "Tar Pits"]
+extra_regions = ["Desert", "Tropical", "Volcanic", "Polar", "Gemstone", "Mountainous", "Cherry Forest", "Swamp", "Jungle", "Pine Forest", "Orchard", "Plains", "Glacier Caves", "Tar Pits", "Candy", "Christmas"]
 monsters = ["Goblin","Troll","Slime"]
 events = ["Chest","Golden Chest"]
 fillers = ["Barrel of Mead", "Draugr's Bone", "Jotun's Toenail", "Nidhogg's Claw", "Shard of the Bifrost", "Jormungandr's Scale", "Fenrir's Fang", "Leaf of Yggdrasil", "Root of Yggdrasil", "Ratatoskr's Acorn", "Hugin's Feather", "Odin's Eyepatch", "Fafnir's Coin", "Baldur's Arrow", "Heimdall's Horn", "Lock of Freya's Hair", "Loki's Dagger", "Thor's Spare Hammer", "Sleipnir's Saddle"]
