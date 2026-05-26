bc38e647bf8516ddf57c28267d0cdf144d89a546
Nikkilite <Niko6697n@gmail.com>
2026-05-26 16:01:27 +0200

    Add support for 30 islands, 25 Locations and 10 runes

--- options.py
+++ options.py
@@ -8,7 +8,7 @@ class PrioritizedBacklog(OptionList):
     """
     Games that will be your goal on an island.
     These games will always be included, and generate as many locations as defined here.
-    Maximum of 20 games allowed, with a maximum of 20 locations per game.
+    Maximum of 30 games allowed, with a maximum of 25 locations per game.
     """
 
     display_name = "Prioritized Backlog"
@@ -28,7 +28,7 @@ class RandomizedBacklog(OptionList):
     """
     Games that will be your goal on an island.
     These games will be randomly picked, based on your "Randomized Backlog Amount" option, and will generate as many locations as defined here.
-    You can put an unlimited amount of games here, with a maximum of 20 locations per game.
+    You can put an unlimited amount of games here, with a maximum of 25 locations per game.
     """
 
     display_name = "Randomized Backlog"
@@ -96,26 +96,26 @@ class TreasuresToGoal(Range):
     """
     How many Treasures you need to find to Goal.
     On each island you will find a treasure, so this number can only go as high as your "Number Of Islands" option.
-    A maximum of 20, and minimum of 1.
+    A maximum of 30, and minimum of 1.
     """
 
     display_name = "Treasures To Goal"
 
     range_start = 1
-    range_end = 20
+    range_end = 30
     default = 3
 
 class  RandomizedBacklogAmount(Range):
     """
     How many random backlog games your world will have, in addition to your prioritized backlog games.
     This number cannot go higher than the amount of games in your "Randomized Backlog" option
-    A maximum of 20.
+    A maximum of 30.
     """
 
     display_name = "Randomized Backlog Amount"
 
     range_start = 0
-    range_end = 20
+    range_end = 30
     default = 1
 
 class  NumberOfIslands(Range):
@@ -123,13 +123,13 @@ class  NumberOfIslands(Range):
     How many Islands your world will have.
     Make sure that this number is either equal to or higher than the amount of games in your "Prioritized Backlog" option + your "Randomized Backlog Amount" option.
     An Island that is not filled with a Backlog Game will instead become a medley island, filled with random objectives from the other location lists.
-    A maximum of 20, and minimum of 1.
+    A maximum of 30, and minimum of 1.
     """
 
     display_name = "Number Of Islands"
 
     range_start = 1
-    range_end = 20
+    range_end = 30
     default = 4
 
 class  LocationsPerIsland(Range):
@@ -137,25 +137,25 @@ class  LocationsPerIsland(Range):
     How many Locations each island should try to be filled with.
     If no repeatable locations are added, generation will fill up as much as possible.
     Locations needed for games put in any of the Backlog options are guaranteed to exist though.
-    A maximum of 20.
+    A maximum of 25.
     """
 
     display_name = "Locations Per Island"
 
     range_start = 0
-    range_end = 20
+    range_end = 25
     default = 10
 
 class  RunesRequired(Range):
     """
     How many Runes each island will require to be unlocked.
-    A maximum of 5.
+    A maximum of 10.
     """
 
     display_name = "Runes Required"
 
     range_start = 1
-    range_end = 5
+    range_end = 10
     default = 2
 
 class RandomIslandOrder(Toggle):
