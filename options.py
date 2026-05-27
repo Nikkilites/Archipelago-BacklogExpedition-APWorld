e1c1061b17a3a4efd67c43abe5292e105b8898a8
Nikkilite <Niko6697n@gmail.com>
2026-05-27 03:14:02 +0200

    Change option description

--- options.py
+++ options.py
@@ -168,7 +168,7 @@ class RandomIslandOrder(Toggle):
 
 class ForceStartingIslandContent(Toggle):
     """
-    If True: Your starting island is guaranteed to have the first object listed in you "Prioritized Backlog" option
+    If True: Your starting island is guaranteed to have the first game listed in you "Prioritized Backlog" option
     """
     display_name = "Force Starting Island Content"
     default = False
